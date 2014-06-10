807af4c997bcb2ad8dfe73c58dbcd3bce7c1232b
Joao Carlos <jhonny.bittencourt@gmail.com>
2014-06-09 21:02:47 -0300

    - Fix I/O pins definitions acordingly to the specification; - Header update

--- rtl/decode/register_bank.v
+++ rtl/decode/register_bank.v
@@ -1,63 +1,82 @@
-//==================================================================================================
-//  Filename      : register_bank.v
-//  Created On    : 2014-06-06 21:35:51
-//  Last Modified : 2014-06-06 22:04:38
-//  Revision      : 
-//  Author        : Linton Thiago Costa Esteves
-//  Company       : Universidade Federal da Bahia - UFBA
-//  Email         : lintonthiago@gmail.com
+// +----------------------------------------------------------------------------
+// GNU General Public License
+// -----------------------------------------------------------------------------
+// This file is part of uDLX (micro-DeLuX) soft IP-core.
 //
-//  Description   : 
+// uDLX is free soft IP-core: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
+// uDLX soft core is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 //
-//==================================================================================================
+// You should have received a copy of the GNU General Public License
+// along with uDLX. If not, see <http://www.gnu.org/licenses/>.
+// +----------------------------------------------------------------------------
+// PROJECT: uDLX core Processor
+// ------------------------------------------------------------------------------
+// FILE NAME        : register_bank.v
+// AUTHOR(s)        : lintaum
+// MANTAINER        : lintaum, joaocarlos
+// AUTHOR'S E-MAIL  : lintonthiago@gmail.com
+// -----------------------------------------------------------------------------
+// KEYWORDS: decoder, registers, dlx
+// -----------------------------------------------------------------------------
+// PURPOSE: General Purpose Registers of uDLX core
+// -----------------------------------------------------------------------------
 module register_bank
-               #(
-                  DATA_WIDTH = 32,
-                  MEMORY_SIZE = 16,
-                  ADDRESS_WIDTH = 4
-               )
-               (/*autoport*/
-                  input clk,
-                  input rst_n,
-                  input [ADDRESS_WIDTH-1:0] read_address_1,
-                  input [ADDRESS_WIDTH-1:0] read_address_2,
-                  input [ADDRESS_WIDTH-1:0] write_address,
-                  input write_enable,
-                  input [DATA_WIDTH-1:0] write_data,
-                  output [DATA_WIDTH-1:0] read_data_1,
-                  output [DATA_WIDTH-1:0] read_data_2
-               );
-//*******************************************************
-//Internal
-//*******************************************************
-//Local Parameters
+#(
+   DATA_WIDTH = 32,
+   ADDRESS_WIDTH = 4
+)
+(/*autoport*/
+   input clk,
+   input rst_n,
+   input [ADDRESS_WIDTH-1:0] rd_reg1_addr,
+   input [ADDRESS_WIDTH-1:0] rd_reg2_addr,
+   input [ADDRESS_WIDTH-1:0] mem_wb_reg_addr,
+   input mem_wb_reg_en,
+   input [DATA_WIDTH-1:0] mem_wb_reg_data,
+   output [DATA_WIDTH-1:0] rd_reg1_data_out,
+   output [DATA_WIDTH-1:0] rd_reg2_data_out
+);
 
-//Wires
+   // ------------------------------------------------------
+   // Internal
+   // ------------------------------------------------------
+   // Local Parameters
 
-//Registers
-reg [DATA_WIDTH-1:0] mem [0:MEMORY_SIZE-1];
+   // Wires
 
-integer i;
-//*******************************************************
-//General Purpose Signals
-//*******************************************************
-always @(posedge clk or negedge rst_n) begin
-   if (!rst_n) begin
-      for (i = 0; i < MEMORY_SIZE; i = i + 1) begin:reset_memory
-         mem <= {DATA_WIDTH{1'b0}};
+   // Registers
+   reg [DATA_WIDTH-1:0] reg_file [0:(2**ADDRESS_WIDTH)-1];
+
+   // Variables
+   integer i;
+
+   // ------------------------------------------------------
+   // General Purpose Registers
+   // ------------------------------------------------------
+   always @(posedge clk or negedge rst_n) begin
+      if (!rst_n) begin
+         for (i = 0; i < 2**ADDRESS_WIDTH; i = i + 1) begin:reset_memory
+            mem <= {DATA_WIDTH{1'b0}};
+         end
+      end
+      else begin
+         if (write_enable)
+            mem[mem_wb_reg_addr] <= mem_wb_reg_data;
       end
    end
-   else begin
-      if (write_enable)
-         mem[write_address] <= write_data;
-   end
-end
-//*******************************************************
-//Outputs
-//*******************************************************
 
-assign read_data_2 = mem[read_address_2];
-assign read_data_1 = mem[read_address_1];
+   // ------------------------------------------------------
+   // Output registers
+   // ------------------------------------------------------
+
+   assign rd_reg2_data_out = reg_file[rd_reg2_addr];
+   assign rd_reg1_data_out = reg_file[rd_reg1_addr];
 
 endmodule

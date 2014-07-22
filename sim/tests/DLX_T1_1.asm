26b198f5d5c5648f19e2cc1aef12209d2d1712db
Igo Amauri Luz <igoamauri@gmail.com>
2014-07-22 00:37:48 -0300

    Add Tests
    Add the follow tests: DLX_T1_1, DLX_T1_2, DLX_T1_3, DLX_T1_4

---
+++ sim/tests/DLX_T1_1.asm
@@ -0,0 +1,43 @@
+addi $0, $0, 0
+nop
+nop
+nop
+nop
+nop
+addi $1, $0, 1
+addi $2, $0, 2
+addi $3, $0, 3
+addi $4, $0, 4
+addi $5, $0, 5
+addi $6, $0, 6
+addi $7, $0, 7
+addi $8, $0, 8
+addi $9, $0, 9
+addi $10, $0, 10
+addi $11, $0, 11
+addi $12, $0, 12
+addi $13, $0, 13
+addi $14, $0, 14
+addi $15, $0, 15
+addi $16, $0, 16
+addi $17, $0, 17
+addi $18, $0, 18
+addi $19, $0, 19
+addi $20, $0, 20
+addi $21, $0, 21
+addi $22, $0, 22
+addi $23, $0, 23
+addi $24, $0, 24
+addi $25, $0, 25
+addi $26, $0, 26
+addi $27, $0, 27
+addi $28, $0, 28
+addi $29, $0, 29
+addi $30, $0, 30
+addi $31, $0, 31
+nop
+nop
+nop
+nop
+nop
+nop

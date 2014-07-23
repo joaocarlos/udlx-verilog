12c2d6b1e54c9d7f91894438ed009d1cb261fc5c
victorvalente <victor.valente.araujo@gmail.com>
2014-07-23 05:47:24 -0300

    linton - make the read operation sync with the processor clock

--- rtl/inst_memory_controll/sram_ctrl.v
+++ rtl/inst_memory_controll/sram_ctrl.v
@@ -2,12 +2,13 @@ module sram_ctrl (
     
     input clk,
     input rst_n,
+    input clk_proc,
     
     input wr_en,
     input rd_en,
     input [31:0] wr_data,
     input [20:0] addr,
-    output [31:0] rd_data,
+    output reg [31:0] rd_data,
 
     output sram_ub_n,
     output sram_lb_n,
@@ -21,15 +22,27 @@ module sram_ctrl (
 
 parameter ST_IDLE    = 0;
 parameter ST_WRITE_0 = 1;
-parameter ST_READ_0  = 2;
-parameter ST_READ_1  = 3;
+parameter ST_WRITE_1 = 2;
+parameter ST_READ_0  = 3;
+parameter ST_READ_1  = 4;
+parameter ST_READ_2  = 5;
 
-reg [1:0] state;
-reg [1:0] next_state;
+reg [2:0] state;
+reg [2:0] next_state;
+
+reg [2:0]  counter;
 
 reg [31:0] rd_data_reg;
 wire [20:0] addr_plus2;
-wire [31:0] rd_data_concat;
+
+reg wr_data_dly;
+reg rd_data_dly;
+reg wr_detc;
+//wire rd_detc;
+reg clk_proc_pulse;
+reg clk_proc_dly;
+
+//wire [31:0] rd_data_concat;
 
 assign sram_ub_n = 1'b0;
 assign sram_lb_n = 1'b0;
@@ -37,60 +50,105 @@ assign sram_lb_n = 1'b0;
 assign addr_plus2 = addr+2;
 
 
-assign rd_data_concat = {sram_rd_data,rd_data_reg[15:0]};
+//assign rd_data_concat = {sram_rd_data,rd_data_reg[15:0]};
 
-assign rd_data = (state==ST_READ_1)?rd_data_concat:rd_data_reg;
-
-
-always@(*)begin
-   if(wr_en)begin //write
-      sram_ce_n = 1'b0;
-      sram_we_n = 1'b0;
-      sram_oe_n = 1'b1;
-      if(state==ST_IDLE)begin
-	 sram_wr_data = wr_data[15:0];
-	 sram_addr = addr[20:1];
-      end
-      else begin
-	 sram_wr_data = wr_data[31:16];
-	 sram_addr = addr_plus2[20:1];
-      end
-   end
-   else if (rd_en) begin//read
-      sram_ce_n = 1'b0;
-      sram_we_n = 1'b1;
-      sram_oe_n = 1'b0;
-      if(state==ST_IDLE||state==ST_READ_1)begin
-	 sram_addr = addr[20:1];
-      end
-      else begin
-	 sram_addr = addr_plus2[20:1];
-      end
-   end
-   else begin
-      sram_ce_n = 1'b1;
-      sram_we_n = 1'b1;
-      sram_oe_n = 1'b0;
-      sram_addr = 20'd0;
-      sram_wr_data = 16'd0;
-   end
+always@(posedge clk, negedge rst_n)begin
+  if(!rst_n)begin
+    rd_data <= 0;
+  end
+  else begin
+    if(state == ST_IDLE)begin
+      rd_data <= rd_data_reg;
+    end
+  end
 end
 
-
+always@(posedge clk, negedge rst_n)begin
+   if(!rst_n)begin
+      wr_data_dly <= 1'b0;
+      clk_proc_dly <= 1'b0;
+      wr_detc <= 1'b0;
+      clk_proc_pulse <= 1'b0;
+   end
+   else begin
+      wr_data_dly <= wr_en;
+      wr_detc <= wr_en & !wr_data_dly;
+      clk_proc_dly <= clk_proc;
+      clk_proc_pulse <= clk_proc & !clk_proc_dly;
+   end
+end
 
 always@(posedge clk, negedge rst_n)begin
    if(!rst_n)begin
       rd_data_reg <= 32'd0;
+      sram_ce_n = 1'b0;
+      sram_we_n = 1'b0;
+      sram_oe_n = 1'b1;
+      sram_wr_data = 0;
+      sram_addr = 0;
    end
    else begin
       case(state)
+	 ST_IDLE: begin
+            if(wr_detc)begin //write
+               sram_ce_n <= 1'b0;
+               sram_we_n <= 1'b0;
+               sram_oe_n <= 1'b1;
+               sram_wr_data <= wr_data[15:0];
+               sram_addr <= addr[20:1];
+            end
+            else if (rd_en && clk_proc_pulse) begin//read
+               sram_ce_n <= 1'b0;
+               sram_we_n <= 1'b1;
+               sram_oe_n <= 1'b0;
+               sram_addr <= addr[20:1];
+            end
+            else begin
+               sram_ce_n <= 1'b1;
+               sram_we_n <= 1'b1;
+               sram_oe_n <= 1'b1;
+               sram_addr <= 20'd0;
+               sram_wr_data <= 16'd0;
+            end
+	 end
+         ST_WRITE_0: begin
+            sram_ce_n <= 1'b1;
+            sram_we_n <= 1'b1;
+            sram_oe_n <= 1'b1;
+	    sram_wr_data <= 0;
+            sram_addr <= 0;
+	 end
+         ST_WRITE_1: begin
+            sram_ce_n <= 1'b0;
+            sram_we_n <= 1'b0;
+            sram_oe_n <= 1'b1;
+	    sram_wr_data <= wr_data[31:16];
+            sram_addr <= addr_plus2[20:1];
+	 end
          ST_READ_0: begin
-            rd_data_reg[15:0] <= sram_rd_data;
+            sram_ce_n <= 1'b1;
+            sram_we_n <= 1'b1;
+            sram_oe_n <= 1'b0;
+            sram_addr <= 0;
          end
          ST_READ_1: begin
-            rd_data_reg[31:16] <= sram_rd_data;
+            sram_ce_n <= 1'b0;
+            sram_we_n <= 1'b1;
+            sram_oe_n <= 1'b0;
+            sram_addr <= addr_plus2[20:1];
+            rd_data_reg[15:0] <= sram_rd_data;
          end
+         ST_READ_2: begin
+            sram_ce_n <= 1'b1;
+            sram_we_n <= 1'b1;
+            sram_oe_n <= 1'b0;
+            sram_addr <= addr_plus2[20:1];
+            rd_data_reg[31:16] <= sram_rd_data;
+	 end
          default: begin
+            sram_ce_n <= 1'b1;
+            sram_we_n <= 1'b1;
+            sram_oe_n <= 1'b1;
             rd_data_reg <= rd_data_reg;
          end
       endcase
@@ -110,26 +168,27 @@ end
 always@(*)begin
    case (state)
       ST_IDLE: begin
-       if(wr_en)
+       if(wr_detc)
           next_state = ST_WRITE_0;
-       else if(rd_en)
+       else if(rd_en && clk_proc_pulse)
           next_state = ST_READ_0;
        else
           next_state = ST_IDLE;
       end
       ST_WRITE_0: begin
+         next_state = ST_WRITE_1;
+      end
+      ST_WRITE_1: begin
          next_state = ST_IDLE;
       end
       ST_READ_0: begin
          next_state = ST_READ_1;
       end
       ST_READ_1: begin
-       if(wr_en)
-          next_state = ST_WRITE_0;
-       else if(rd_en)
-          next_state = ST_READ_0;
-       else
-          next_state = ST_IDLE;
+         next_state = ST_READ_2;
+      end
+      ST_READ_2: begin
+        next_state = ST_IDLE;
       end
       default:begin
          next_state = ST_IDLE;

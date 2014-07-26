e934344a052d5339a5892228f5b62bb471d67abd
Igo Amauri Luz <igoamauri@gmail.com>
2014-07-26 00:22:41 -0300

    Add new tests
    Add new tests: DLX_T3_1, DLX_T3_2, DLX_T3_3, DLX_T3_4 and update
    DLX_T1_2

---
+++ sim/tests/DLX_T3_1.asm
@@ -0,0 +1,20 @@
+addi $10, $0,0x0f
+addi $11, $0,0xff
+addi $12, $0,0xfff
+addi $13, $0,0x3fff
+addi $14, $0,0x30f0
+addi $15, $0,0x3000
+addi $16, $0,0x3ff0
+andi $1, $10, 0xf9
+andi $2, $11, 0xf99
+andi $3, $12, 0x30b0
+andi $4, $13, 0x3dbd
+andi $5, $14, 0xf0f
+andi $6, $15, 0x3669
+andi $7, $16, 0x76f
+nop
+nop
+nop
+nop
+nop
+nop
---
+++ sim/tests/DLX_T3_3.asm
@@ -0,0 +1,32 @@
+addi $10, $0,0x0f
+addi $11, $0,0xff
+addi $12, $0,0xfff
+addi $13, $0,0x3fff
+addi $14, $0,0x30f0
+addi $15, $0,0x3000
+addi $16, $0,0x3ff0
+addi $1, $0, 0xf9
+addi $2, $0, 0xf99
+addi $3, $0, 0x30b0
+addi $4, $0, 0x3dbd
+addi $5, $0, 0xf0f
+addi $6, $0, 0x3669
+addi $7, $0, 0x76f
+nop
+nop
+nop
+nop
+nop
+and $17, $10, $1
+and $18, $11, $2
+and $19, $12, $3
+and $20, $13, $4
+and $21, $14, $5
+and $22, $15, $6
+and $23, $16, $7
+nop
+nop
+nop
+nop
+nop
+nop
